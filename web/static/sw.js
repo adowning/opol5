48abaa806da8ade775ac8bbd6b1af4356f3943ee
adowning <ashdowning@gmail.com>
2018-05-20 10:52:46 -0500

    adsfadsf

--- web/static/sw.js
+++ web/static/sw.js
@@ -1,15 +1,15 @@
 importScripts('/_nuxt/workbox.3de3418b.js')
 
 const workboxSW = new self.WorkboxSW({
-  "cacheId": "andrews-admin",
+  "cacheId": "nuxt",
   "clientsClaim": true,
   "directoryIndex": "/"
 })
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/3c12a10e2bf3f7638f06.js",
-    "revision": "f6e113f3bc5874879f3fe1b7bc87acec"
+    "url": "/_nuxt/9e84b1ce0c9a9b02c1a4.js",
+    "revision": "a3c5550cb1f32bf353fc341c1bf7282e"
   },
   {
     "url": "/_nuxt/af0f1c8ca79c05086ad4.js",
